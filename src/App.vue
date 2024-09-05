e95836e53ce95783ace0da0cc1fffc4fde6d6573
emmanuelhashy <emmanuelhashy@gmail.com>
2024-09-05 19:07:44 +0100

    Implement pagination component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,37 @@
-<script setup lang="ts">
+<script>
+import PaginationComponent from "./components/pagination/PaginationComponent.vue";
+
+export default {
+  name: "app-component",
+  components: { PaginationComponent },
+  data() {
+    return {
+      pageSize: 2000,
+      currentPage: 1,
+      limit: 6,
+    };
+  },
+  methods: {
+    handlePageChange(page) {
+      this.currentPage = Number(page);
+    },
+  },
+};
 </script>
 
 <template>
-  <div  class="h-vh">
-      <router-view/>
-  </div>
+  <main
+    class="px-[28px] flex justify-center items-center h-screen bg-white sm:px-[48px]"
+  >
+    <PaginationComponent
+      :total-items="pageSize"
+      :current-page="currentPage"
+      :per-page="limit"
+      @page-changed="handlePageChange"
+      :go-button="false"
+      styled="simple"
+    />
+  </main>
 </template>
 
-<style scoped>
-.main {
-  display: flex;
-  justify-content: start;
-  height: 90%;
-}
-.h-vh {
-  height: 100vh;
-}
-@media (min-width: 1024px) {
-
-}
-</style>
+<style scoped></style>
