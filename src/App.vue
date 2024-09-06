bc7916f0f2ec5f31935e3f9cbb4831c8bbc1373d
emmanuelhashy <emmanuelhashy@gmail.com>
2024-09-06 18:47:24 +0100

    Implement file upload component

--- src/App.vue
+++ src/App.vue
@@ -1,87 +1,45 @@
 <script>
-import Modal from "./components/modals/Modal.vue";
-import IconBin from "./components/icons/IconBin.vue"
-import IconEdit from "./components/icons/IconEdit.vue"
+import Upload from "./components/upload/upload.vue"
 export default {
   name: "app-component",
-  components: { Modal, IconBin, IconEdit },
+  components: { Upload },
   data() {
     return {
-      isModalOpen: false,
-      isModal1Open: false,
+      bannerUrl: ""
     };
   },
   methods: {
-    openModal() {
-      this.isModalOpen = true;
-    },
-    openModal1() {
-      this.isModal1Open = true;
-    },
+   getImgURL(val) {
+  this.bannerUrl = val
+}
   },
 };
 </script>
 
 <template>
-  <Modal :open="isModalOpen" @close="isModalOpen = !isModalOpen">
-    <div class="flex w-full justify-center">
-      <IconBin />
-    </div>
-    <div class="px-8 space-y-3">
-          <p class="text-lg font-bold text-center">Are you sure you want to delete </p>
-          <p class="text-base font-normal text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud</p>
-          <div class="flex space-x-6 justify-center">
-              <button @click="isModalOpen = !isModalOpen" class="btn bg-[#E5DBE5] text-[#4B104B]">
-                  <span>No</span>
-              </button>
-              <button @click="isModalOpen = !isModalOpen" class="btn bg-[#4B104B] text-white">
-                  <span>Yes</span>
-              </button>
-          </div>
-      </div>
-  </Modal>
-  <Modal :open="isModal1Open" @close="isModal1Open = !isModal1Open">
-    <div class="flex w-full justify-center">
-      <IconEdit />
-    </div>
-    <div class="px-8 space-y-3">
-          <p class="text-lg font-bold text-center">Are you sure you want to save changes</p>
-          <p class="text-base font-normal text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud</p>
-          <div class="flex space-x-6 justify-center">
-              <button @click="isModal1Open = !isModal1Open" class="btn bg-[#E5DBE5] text-[#4B104B]">
-                  <span>No</span>
-              </button>
-              <button @click="isModal1Open = !isModal1Open" class="btn bg-[#4B104B] text-white">
-                  <span>Yes</span>
-              </button>
-          </div>
-      </div>
-  </Modal>
   <main class="px-[28px] flex justify-center space-x-3 items-center h-screen bg-white sm:px-[48px]">
-    <div class="text-left border space-y-3 py-4 px-8 rounded-md border-[#EEEFF2]">
-      <p class="font-bold text-base">Custom Modals</p>
-      <div class="space-x-2">
-        <button @click="openModal" class="bg-red-700 text-white py-1.5 px-3 rounded-md">
-            <span>Delete Changes</span>
-          </button>
-        <button @click="openModal1" class="bg-green-700 text-white py-1.5 px-3 rounded-md">
-            <span>Save Changes</span>
-          </button>
+    <div class="text-left border space-y-3 py-4 px-8 w-96 rounded-md border-[#EEEFF2]">
+      <p class="font-bold text-base">File Upload Component</p>
+      <div class="space-y-2">
+          <p class="label">Banner Image</p>
+          <div class="input">
+            <Upload :callbackFunc="getImgURL" />
+          </div>
       </div>
     </div>
   </main>
 </template>
 
 <style scoped>
-.btn {
-    padding: 8px 40px;
-    border-radius: 30px;
-    border: none;
-    font-size: 16px;
-    font-weight: 500;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    cursor: pointer;
+.input {
+  width: 100%;
+  height: 3rem;
+  border-radius: 13px;
+  color: #000000;
+  border: 1px solid #b1b1b1;
+  padding-left: 16px;
+  font-size: 14px;
+  display: flex;
+  align-items: center;
 }
 </style>
---
+++ src/components/upload/upload.vue
@@ -0,0 +1,134 @@
+<template>
+    <div class="row">
+        <div class="split">
+            <p class="text-truncate" v-if="imgUrl">{{ imgUrl }}</p>
+            <p v-else>No file</p>
+            <button v-on:click="open" id="upload_widget" class="upload-button">
+                Upload image
+            </button>
+        </div>
+        <p v-if="errorMessage">{{ errorMessage }}</p>
+    </div>
+</template>
+  
+<script>
+  export default {
+    name: 'file-upload',
+  }
+</script>
+<script setup>
+const cloudName = import.meta.env.VITE_CLOUD_NAME;
+const uploadPreset = import.meta.env.VITE_UPLOAD_PRESET;
+import { ref, onMounted } from 'vue';
+
+const props = defineProps({
+    callbackFunc: {
+        type: Function,
+        required: true
+    }
+});
+
+const imgUrl = ref("")
+const errorMessage = ref("")
+
+
+const myWidget = window.cloudinary.createUploadWidget(
+    {
+        cloudName: cloudName,
+        uploadPreset: uploadPreset,
+        cropping: true,
+        theme: "purple",
+        styles: {
+            palette: {
+                window: "#531253",
+                windowBorder: "#4B104B",
+                tabIcon: "#FDE4C5",
+                menuIcons: "#FDE4C5",
+                textDark: "#000000",
+                textLight: "#FFFFFF",
+                link: "#EEE7EE",
+                action: "#4B104B",
+                inactiveTabIcon: "#FDE4C5",
+                error: "#F44235",
+                inProgress: "#0078FF",
+                complete: "#20B832",
+                sourceBg: "#CAB6CA"
+            },
+            frame: {
+                background: "#0E2F5B99"
+            },
+        },
+    },
+    (error, result) => {
+        if (!error && result && result.event === "success") {
+            imgUrl.value = result.info.url
+            props.callbackFunc(result.info.url)
+        }
+        else if (error) {
+            errorMessage.value = error.message;
+        }
+    }
+);
+
+function open() {
+    myWidget.open();
+}
+
+const handleDataReset = () => {
+    imgUrl.value = ""
+}
+
+onMounted(() => {
+      window.addEventListener('resetData', handleDataReset);
+    });
+</script>  
+<style scoped>
+footer > div > img{
+    display:none !important;
+  }
+.bc-link {
+    border-color: #FB8E0B !important;
+}
+
+.bg-link {
+    background-color: red !important;
+}
+
+.upload-button {
+    padding: 10px 10px;
+    background-color: #FB8E0B;
+    border: #FB8E0B;
+    color: white;
+    height: 100%;
+    font-size: 14px;
+    border-top-right-radius: 12px;
+    border-bottom-right-radius: 12px;
+    font-weight: 600;
+    cursor: pointer;
+    white-space: nowrap;
+}
+
+.row{
+    width: 100%;
+    height: 100%;
+}
+
+.text-truncate {
+    overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+
+.split {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    align-items: center;
+    height: 100%;
+}
+
+.img-preview {
+    width: 60px;
+    height: 35px;
+    margin-left: 5px;
+}</style>
